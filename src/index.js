9a0f306842458dac9f2c9872689d3deb0dfa2a58
Shantanu Saha <sansaha0505@gmail.com>
2021-12-15 21:19:54 +0530

    make it better new mmm onemmbbjj

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port =  8080
+const port = process.env.PORT || 8080
 // process.env.PORT ||
 const db = require('./db/db')
 const resgistration = require('./routes/Registration')
