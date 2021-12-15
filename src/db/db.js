d59699e632821a774a7163c0d030bee95858e1c4
Shantanu Saha <sansaha0505@gmail.com>
2021-12-15 21:19:04 +0530

    make it better new mmm onemmbb

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
-// const Pusher = require("pusher");
+const Pusher = require("pusher");
 
 
-// const pusher = new Pusher({
-//     appId: "1232626",
-//     key: "30b83b9b426488afe28c",
-//     secret: "be18487428d2f546e376",
-//     cluster: "ap2",
-//     useTLS: true
-//   });
+const pusher = new Pusher({
+    appId: "1232626",
+    key: "30b83b9b426488afe28c",
+    secret: "be18487428d2f546e376",
+    cluster: "ap2",
+    useTLS: true
+  });
 
 mongoose.connect(" mongodb+srv://sanz:sannu05@cluster0.s5xci.mongodb.net/pos?retryWrites=true&w=majority", {
   
@@ -21,55 +21,55 @@ mongoose.connect(" mongodb+srv://sanz:sannu05@cluster0.s5xci.mongodb.net/pos?ret
     console.log("db failed",err)
 })
 
-// const db = mongoose.connection
-// db.once('open', () => {
-//     console.log('db coonected again')
-//     const msgCollection = db.collection('productdetails');
-//     const changeStream = msgCollection.watch()
-//     changeStream.on('change', (change)=>{
-//         console.log(change)
-//         if(change.operationType==='insert'){
-//             const msgdetail = change.fullDocument;
-//             pusher.trigger('pdtinsert','insert',{
-//                 user:msgdetail.name
-//             })
-//         }
-//         if(change.operationType==='update'){
-//             const msgdetail = change.updateDescription;
-//             pusher.trigger('pdtupdate','update',{
-//                 user:msgdetail
-//             })
-//         }
-//         if(change.operationType==='delete'){
-//             const msgdetail = change.documentKey;
-//             pusher.trigger('pdtdelete','delete',{
-//                 user:msgdetail
-//             })
-//         }
-//     })
-//     console.log('db coonected again')
-//     const msgCollection1 = db.collection('categorys');
-//     const changeStream1 = msgCollection1.watch()
-//     changeStream1.on('change', (change)=>{
-//         console.log(change)
-//         if(change.operationType==='insert'){
-//             const msgdetail = change.fullDocument;
-//             pusher.trigger('pdtinsert','insert',{
-//                 user:msgdetail.name
-//             })
-//         }
-//         if(change.operationType==='update'){
-//             const msgdetail = change.documentKey;
-//             pusher.trigger('pdtupdate','update',{
-//                 user:msgdetail._id
-//             })
-//         }
-//         if(change.operationType==='delete'){
-//             const msgdetail = change.documentKey;
-//             pusher.trigger('pdtdelete','delete',{
-//                 user:msgdetail
-//             })
-//         }
-//     })
-// })
+const db = mongoose.connection
+db.once('open', () => {
+    console.log('db coonected again')
+    const msgCollection = db.collection('products');
+    const changeStream = msgCollection.watch()
+    changeStream.on('change', (change)=>{
+        console.log(change)
+        if(change.operationType==='insert'){
+            const msgdetail = change.fullDocument;
+            pusher.trigger('pdtinsert','insert',{
+                user:msgdetail.name
+            })
+        }
+        if(change.operationType==='update'){
+            const msgdetail = change.updateDescription;
+            pusher.trigger('pdtupdate','update',{
+                user:msgdetail
+            })
+        }
+        if(change.operationType==='delete'){
+            const msgdetail = change.documentKey;
+            pusher.trigger('pdtdelete','delete',{
+                user:msgdetail
+            })
+        }
+    })
+    console.log('db coonected again')
+    const msgCollection1 = db.collection('customers');
+    const changeStream1 = msgCollection1.watch()
+    changeStream1.on('change', (change)=>{
+        console.log(change)
+        if(change.operationType==='insert'){
+            const msgdetail = change.fullDocument;
+            pusher.trigger('pdtinsert','insert',{
+                user:msgdetail.name
+            })
+        }
+        if(change.operationType==='update'){
+            const msgdetail = change.documentKey;
+            pusher.trigger('pdtupdate','update',{
+                user:msgdetail._id
+            })
+        }
+        if(change.operationType==='delete'){
+            const msgdetail = change.documentKey;
+            pusher.trigger('pdtdelete','delete',{
+                user:msgdetail
+            })
+        }
+    })
+})
 
